dde111fda1d7ae8c1118a3f95ca244b890addaa6
VitaBabenko <babenkovita1988@gmail.com>
2023-10-14 19:47:58 +0300

    done

--- dist/index.3d214d75.js
+++ dist/index.3d214d75.js
@@ -575,47 +575,82 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"bB7Pu":[function(require,module,exports) {
-// const input = document.querySelector(".filter");
+var _styleCss = require("./style.css");
+const input = document.querySelector(".filter");
 const list = document.querySelector(".list_posts");
-const loadMoreBtn = document.querySelector(".load_more");
+const loader = document.querySelector(".loader");
 let page = 1;
-let renderedPosts = 5;
-loadMoreBtn.addEventListener("click", onLoadMoreBtn);
+const limitPerPage = 5;
+const posts = [];
+let isLoading = false;
+let scroll = true;
+document.addEventListener("scroll", onScroll);
+input.addEventListener("input", onInput);
+function onAutoScroll() {
+    scroll = true;
+}
+function offAutoScroll() {
+    scroll = false;
+}
 async function fetchPosts(page) {
     try {
         const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
-        const resp = await fetch(`${BASE_URL}?_limit=5&_page=${page}`);
+        const resp = await fetch(`${BASE_URL}?_limit=${limitPerPage}&_page=${page}`);
         return await resp.json();
     } catch (error) {
         console.log(error);
     }
 }
-function createMarkupList(posts) {
-    const markup = posts.map(({ id, title, body })=>{
-        return `<li class="list_item"><span>${id}</span><h2>${title}</h2><p>${body}</p></li>`;
+async function loadPosts() {
+    try {
+        const resp = await fetchPosts(page);
+        posts.push(...resp);
+        createMarkupList(resp);
+        page++;
+    } catch (error) {
+        console.log(error);
+    }
+}
+function createMarkupList(data) {
+    const markup = data.map(({ id, title, body })=>{
+        return `<li class="list_item"><span class="span">${id}</span><h2>${title}</h2><p>${body}</p></li>`;
     }).join("");
     list.insertAdjacentHTML("beforeend", markup);
 }
-fetchPosts(page).then((posts)=>{
-    console.log(posts);
-    createMarkupList(posts);
-}).catch((error)=>console.log(error));
-async function onLoadMoreBtn() {
-    try {
-        page += 1;
-        const response = await fetchPosts(page);
-        renderedPosts += response.length;
-        console.log(renderedPosts);
-        if (renderedPosts === 100) loadMoreBtn.style.display = "none";
-        createMarkupList(response);
-        console.log(response);
-        console.log(page);
-        return;
-    } catch (err) {
-        console.log(err);
+loadPosts();
+function onScroll() {
+    if (scroll && !isLoading && window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        isLoading = true;
+        loader.style.display = "block";
+        setTimeout(()=>{
+            loadPosts().then(()=>{
+                isLoading = false;
+                loader.style.display = "none";
+            });
+        }, 1000);
+    }
+}
+function filterPosts(keyword) {
+    const filteredPosts = posts.filter((post)=>{
+        const titleKeyword = post.title.toLowerCase().includes(keyword.toLowerCase());
+        const bodyKeyword = post.body.toLowerCase().includes(keyword.toLowerCase());
+        return titleKeyword || bodyKeyword;
+    });
+    list.innerHTML = "";
+    createMarkupList(filteredPosts);
+}
+function onInput() {
+    const keyword = input.value;
+    if (keyword.trim() === "") {
+        list.innerHTML = "";
+        createMarkupList(posts);
+        onAutoScroll();
+    } else {
+        filterPosts(keyword);
+        offAutoScroll();
     }
 }
 
-},{}]},["3smKr","bB7Pu"], "bB7Pu", "parcelRequire29af")
+},{"./style.css":"dRy26"}],"dRy26":[function() {},{}]},["3smKr","bB7Pu"], "bB7Pu", "parcelRequire29af")
 
 //# sourceMappingURL=index.3d214d75.js.map
--- index.js
+++ index.js
@@ -1,53 +1,100 @@
-// const input = document.querySelector(".filter");
-const list = document.querySelector(".list_posts");
-const loadMoreBtn = document.querySelector(".load_more");
-let page = 1;
-let renderedPosts = 5;
+import "./style.css";
 
-loadMoreBtn.addEventListener("click", onLoadMoreBtn);
+const input = document.querySelector(".filter");
+const list = document.querySelector(".list_posts");
+const loader = document.querySelector(".loader");
+
+let page = 1;
+const limitPerPage = 5;
+const posts = [];
+let isLoading = false;
+let scroll = true;
+
+document.addEventListener("scroll", onScroll);
+input.addEventListener("input", onInput);
+
+function onAutoScroll() {
+  scroll = true;
+}
+
+function offAutoScroll() {
+  scroll = false;
+}
 
 async function fetchPosts(page) {
   try {
     const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
 
-    const resp = await fetch(`${BASE_URL}?_limit=5&_page=${page}`);
+    const resp = await fetch(
+      `${BASE_URL}?_limit=${limitPerPage}&_page=${page}`
+    );
     return await resp.json();
   } catch (error) {
     console.log(error);
   }
 }
 
-function createMarkupList(posts) {
-  const markup = posts
+async function loadPosts() {
+  try {
+    const resp = await fetchPosts(page);
+    posts.push(...resp);
+    createMarkupList(resp);
+    page++;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createMarkupList(data) {
+  const markup = data
     .map(({ id, title, body }) => {
-      return `<li class="list_item"><span>${id}</span><h2>${title}</h2><p>${body}</p></li>`;
+      return `<li class="list_item"><span class="span">${id}</span><h2>${title}</h2><p>${body}</p></li>`;
     })
     .join("");
   list.insertAdjacentHTML("beforeend", markup);
 }
 
-fetchPosts(page)
-  .then((posts) => {
-    console.log(posts);
+loadPosts();
+
+function onScroll() {
+  if (
+    scroll &&
+    !isLoading &&
+    window.innerHeight + window.scrollY >= document.body.offsetHeight
+  ) {
+    isLoading = true;
+    loader.style.display = "block";
+
+    setTimeout(() => {
+      loadPosts().then(() => {
+        isLoading = false;
+        loader.style.display = "none";
+      });
+    }, 1000);
+  }
+}
+
+function filterPosts(keyword) {
+  const filteredPosts = posts.filter((post) => {
+    const titleKeyword = post.title
+      .toLowerCase()
+      .includes(keyword.toLowerCase());
+    const bodyKeyword = post.body.toLowerCase().includes(keyword.toLowerCase());
+    return titleKeyword || bodyKeyword;
+  });
+
+  list.innerHTML = "";
+  createMarkupList(filteredPosts);
+}
+
+function onInput() {
+  const keyword = input.value;
+  if (keyword.trim() === "") {
+    list.innerHTML = "";
     createMarkupList(posts);
-  })
-  .catch((error) => console.log(error));
-
-async function onLoadMoreBtn() {
-  try {
-    page += 1;
-    const response = await fetchPosts(page);
-    renderedPosts += response.length;
-    console.log(renderedPosts);
-    if (renderedPosts === 100) {
-      loadMoreBtn.style.display = "none";
-    }
-    createMarkupList(response);
-    console.log(response);
-    console.log(page);
-
-    return;
-  } catch (err) {
-    console.log(err);
+    onAutoScroll();
+  } else {
+    filterPosts(keyword);
+    offAutoScroll();
   }
 }
